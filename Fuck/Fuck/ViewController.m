9846533a221f3fa503fb1ab3dce6d0aaed28f020
lkjy <951147663@qq.com>
2016-02-29 21:02:09 +0800

    nhbhjbjh

---
+++ Fuck/Fuck/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Fuck
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 lkjy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"小李");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
